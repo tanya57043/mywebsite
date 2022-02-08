4190a9c5743b41e377f9d53e62d5ab6327d2e221
tanya57043 <97937616+tanya57043@users.noreply.github.com>
2022-02-08 14:19:27 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>คอลัมน์ที่ 1</td>
+        <td>คอลัมน์ที่ 2</td>
+      </tr>
+    </table>
+  </body> 
+</html>
