b892e3ceb420ea8b13194ff2eb6bcd6d80807358
tanya57043 <97937616+tanya57043@users.noreply.github.com>
2022-02-10 22:17:30 +0700

    Update table.php

--- table.php
+++ table.php
@@ -8,12 +8,46 @@
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
       </tr>
       <tr>
         <td>1</td>
-        <td>หทัยพัชร โตพุ่ม</td>
-        <td>ธัญญ่า</td>
-      </tr>  
+        <td>kim jisoo</td>
+        <td>jisoo</td>
+        <td><img src="https://i.pinimg.com/originals/c5/0a/46/c50a46f12c99102d113ed344fa6390d0.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>2</td>
+        <td>kim jennie</td>
+        <td>jennie</td>
+        <td><img src="https://i.pinimg.com/736x/7d/e1/0b/7de10be8df846f5531abcceeb448ef8c.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>3</td>
+        <td>park chaeyoung</td>
+        <td>rosé</td>
+        <td><img src="https://www.brighttv.co.th/wp-content/uploads/2021/03/rose-blackpink-thanks-fans%E0%B8%9B%E0%B8%81.png" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>4</td>
+        <td>lalisa manobal</td>
+        <td>lisa</td>
+        <td><img src="https://s359.kapook.com/pagebuilder/a842997f-38e7-4be4-aa8c-eedfc2efd45c.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>5</td>
+        <td>Nootsara Tomkom</td>
+        <td>Noot</td>
+        <td><img src="https://images.app.goo.gl/GEx4rtnqij4sbDFw8" width="100px"></td>
+        <td><a href="https://twitter.com/volleyballworld?s=21">twitter</a></td>
+        <tr>
+        <td>6</td>
+        <td>Kim yeonkoung</td>
+        <td>Yeonkoung</td>
+        <td><img src="https://images.app.goo.gl/XZPxq9xEHdDq2kjQA" width="100px"></td>
+        <td><a href="https://twitter.com/kimyk10?s=21">twitter</a></td>
+      </tr>
     </table>
   </body> 
 </html>
