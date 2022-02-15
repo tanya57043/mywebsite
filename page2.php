3dba371bb63f719ea33d1f825d00e9ad2447506b
tanya57043 <97937616+tanya57043@users.noreply.github.com>
2022-02-15 14:19:57 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,8 +1,24 @@
 <html>
 	<head>
 		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: blue;
+			}
+			body{
+                                background-color: lightpink;
+			}
+			h1{
+				color: yellow;      
+		</style>
 	</head>
 	<body>
-    <p style="color:pink;">ข้อความนี้สีชมพู</p>
+		<div id="box1" class="box">
+			<h1>นางสาวหทัยพัชร โตพุ่ม ชั้น 4/13 เลขที่ 24</h1>
+		</div>
 	</body>
 </html>
