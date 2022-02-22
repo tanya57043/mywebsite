9c43681485cf8a1c1a23f2b83a8342c04077aa44
tanya57043 <97937616+tanya57043@users.noreply.github.com>
2022-02-22 16:00:12 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url()
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url(Doughnut.woff)
+    }
+    #font1{
+	font-family: myFont1;
+    }
+    #font2{
+	font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวหทัยพัชร โตพุ่ม ม.4/13 เลขที่ 24 เลขประจำตัว 57043</h1>
+  <h1 id="font2">นางสาวหทัยพัชร โตพุ่ม ม.4/13 เลขที่ 24 เลขประจำตัว 57043</h1>
+</body>
+</html>
